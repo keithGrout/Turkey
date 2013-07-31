83b53a9dc2d351372a563e7797781249b41dfa8b
Keith <user@Keiths-Macbook-Pro.local>
2013-07-31 02:11:18 -0700

    Initial Commit

---
+++ Turkey/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Turkey
+//
+//  Created by Keith on 7/31/13.
+//  Copyright (c) 2013 Appology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
